5fff968ad5224e726f217d9a337d21e792b09985
Declan <declan.h@gmail.com>
2017-08-24 23:26:44 +0100

    added more loggng, added pushover

--- traktToSonarr.py
+++ traktToSonarr.py
@@ -2,19 +2,23 @@ import json
 import requests
 import time
 import logging
+import sys
 
 ################################
 # config
 ################################
 
-timer = 1  # how often (in hours) to check for new shows and add them
+timer = 24  # how often (in hours) to check for new shows and add them
 traktAPI = ''  # API from your trakt account
 sonarrAPI = ''  # API from your sonarr install
 traktLimit = '100'  # how many results to request from trakt's list
-listName = 'anticipated'  # Trending, popular or anticipated
+listName = 'trending'  # Trending or anticipated
 sonarr = 'http://localhost:8989'  # URL to sonarr install, normally localhost:8989
-quality_profile = 'any'  # Sonarr quality profile to add shows under
-folder_path = '/tv/'  # Root folder to download tv shows into
+quality_profile = ''  # Sonarr quality profile to add shows under
+folder_path = ''  # Root folder to download tv shows into
+
+pushover_user_token = ''
+pushover_app_token = ''
 
 # Optional filters
 tRatings = ''  # Only return results which have Trakt ratings within set range, e.g. 70-100
@@ -22,22 +26,36 @@ tGenres = ''  # Only return results within specified genres, e.g. action, advent
 tLang = 'en'  # Only return results in set language, e.g en or es
 tYears = ''  # Only return results from year, or year range, e.g. 2007 or 2007-2015
 tCountries = ''  # Only return results from country, e.g. us
-tRuntimes = '30-240'  # Only return results where shows have a runtime within range, e.g. 30-60
+tRuntimes = '30-60'  # Only return results where shows have a runtime within range, e.g. 30-60
 
 ################################
 # setup
 ################################
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', filename='sonarrPush.log', level=logging.DEBUG)
+logging.basicConfig(stream=sys.stdout, format='%(asctime)s - %(levelname)s: %(message)s', filename='sonarrPush.log',
+                    level=logging.INFO)
 sonarrHeaders = {'X-Api-Key': sonarrAPI}
+pushHeaders = {'Content-Type": "application/x-www-form-urlencoded', 'Content-Length: 180'}
 traktHeaders = {'content-type': 'application/json', 'trakt-api-version': '2', 'trakt-api-key': traktAPI, }
 options = {"ignoreEpisodesWithFiles": False, "ignoreEpisodesWithoutFiles": False, "searchForMissingEpisodes": True}
 sent = None
 newShows = []
+delay_time = timer * 3600
 
+
+def send_pushover(app_token, user_token, message):
+    try:
+        payload = {'token': app_token, 'user': user_token, 'title': 'Sonarr Push', 'message': message}
+        r = requests.post('https://api.pushover.net/1/messages.json', data=payload, headers={'User-Agent': 'Python'})
+        resp = r.status_code
+        logging.debug('sending notifcation to pushover')
+        return True if resp == 200 else False
+    except:
+        logging.warning("error sending notification to %r", user_token)
+        return False
 
 def trakt_url():
-    turl = "https://api.trakt.tv" + "/shows/%s/?limit=%s" % (listName, traktLimit)
+    turl = "https://api.trakt.tv" + "/shows/%s/?limit=%s" % (listName.lower(), traktLimit)
     if tRatings:
         turl += '&ratings=' + tRatings
     if tGenres:
@@ -68,8 +86,8 @@ def qprofile_lookup():
 def sonarr_lib():
     """get sonarr library in a list of tvdbid ids"""
     r = requests.get(sonarr + '/api/series', headers=sonarrHeaders)
-    tvLibList = []
     global tvLibList
+    tvLibList = []
     tv_lib_raw = r.json()
     for n in tv_lib_raw:
         tvLibList.append(n['tvdbId'])
@@ -79,15 +97,19 @@ def sonarr_lib():
 def get_trakt():
     """get trakt list info"""
     r = requests.get(trakt_url(), headers=traktHeaders)
+    global traktList
+    traktList = []
     if r.status_code == requests.codes.ok:
-        return r.json()
+        traktList = r.json()
+        return True
     else:
         logging.error('Trakt list request failed, is trakt.tv down?')
+        return False
 
 
-def send_to_sonarr():
+def send_to_sonarr(a, b):
     """send found tv program to sonarr"""
-    payload = {"tvdbId": tvdbId, "title": title, "qualityProfileId": qprofile_lookup(), "seasons": [],
+    payload = {"tvdbId": a, "title": b, "qualityProfileId": qprofile_lookup(), "seasons": [],
                "seasonFolder": True, "rootFolderPath": folder_path, "addOptions": options, "images": []}
     r = requests.post(sonarr + '/api/series', headers=sonarrHeaders, data=json.dumps(payload))
     global sent
@@ -101,38 +123,46 @@ def send_to_sonarr():
 
 def add_shows():
     """Check new shows & add missing"""
+    get_trakt()
     sonarr_lib()
-    for x in get_trakt():
+    n = 0
+    added_list = []
+    for x in traktList:
         if x['show']['ids']['tvdb'] not in tvLibList:
-            title = ''
-            tvdbId = ''
-            global tvdbId
-            global title
-            tvdbId = (x['show']['ids']['tvdb'])
-            title = (x['show']['title'])
+            title = x['show']['title']
+            tvdb = x['show']['ids']['tvdb']
             try:
-                logging.debug('send show to sonarr: ' + title)
-                send_to_sonarr()
+                logging.info('send show to sonarr: ' + x['show']['title'])
+                send_to_sonarr(tvdb, title)
                 if sent:
                     logging.info(title + ' has been added to Sonarr')
+                    n += 1
+                    added_list.append(x['show']['title'])
+
                 else:
                     logging.warning(title + ' failed to be added to Sonarr!')
             except:
-                logging.warning('error sending, check tvdb - ID movie: ' + title + ' tvdbid: ' + str(tvdbId))
+                logging.warning('error sending show: ' + title + ' tvdbid: ' + tvdb)
+    if pushover_app_token and pushover_user_token:
+        send_pushover(pushover_app_token, pushover_user_token,
+                      "The following " + str(n) + " TV Show(s) have been added to Sonarr: " + '\n'.join(added_list))
 
 
 def new_check():
     """check for new trakt items in list"""
+    get_trakt()
+    sonarr_lib()
     logging.info('checking for new shows in Trakt list')
-    for x in get_trakt():
+    for x in traktList:
         logging.debug('checking show from list: ' + x['show']['title'])
-        if x['show']['ids']['tvdb'] not in sonarr_lib():
-            logging.info('adding shows')
+        if x['show']['ids']['tvdb'] not in tvLibList:
+            logging.info('new show(s) found, adding shows now')
             add_shows()
-        else:
-            logging.info('no new shows to add, checking again in ' + str(timer) + ' hour(s)')
-            time.sleep(120)
-            new_check()
-
+            break
+    logging.info('no new shows to add, checking again in ' + str(timer) + ' hour(s)')
+    time.sleep(float(delay_time))
+    logging.debug('sleping for ' + str(delay_time) + ' seconds')
+    new_check()
+    logging.debug('sleep over, checking again')
 
 new_check()
